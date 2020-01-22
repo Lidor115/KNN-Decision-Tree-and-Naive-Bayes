5156f7a8079fdf8c11006eedee0b23a28da223bc
ubuntu19new <lidor115>
2020-01-22 07:00:23 -0800

    add comments

--- ID3.py
+++ ID3.py
@@ -1,67 +1,107 @@
 import copy
 import math
-from sys import argv
-
 from Node import Node
-from utils import make_examples, parser, stringMaker_and_label, max_can_eat, dev_train_sep, parseAttributes
+from utils import make_examples, max_can_eat, dev_train_sep, parseAttributes
 
 
 class ID3:
+    """
+    Class ID3 - get attributes (all attributes and the features of the train)
+    the default (yes or no) and the examples(train-set)
+    build the tree as a Decision-tree implementation of ID3
+    """
     def __init__(self, F2I, attributes, default, examples):
         self.F2I = F2I
         self.tree = []
-        self.father = None
         self.attributes = attributes
         self.best = None
         self.default = default
         self.examples = examples
 
     def DTL(self):
+        """
+        main function of the algorithm ID3.
+        works recursive and after all return a full tree of ID3
+        :return: tree
+        """
         C_False, C_True, = self.checkClassification(self.examples)
         total_examples = len(self.examples)
+        #if there are no examples
         if len(self.examples) == 0:
             return self.default
+        #all examples are yes
         elif C_True == total_examples:
             return "yes"
+        #all examples are no
         elif C_False == total_examples:
             return "no"
+        #there are no more attributes
         elif len(self.attributes) == 0:
             res = "no"
             if self.Mode(C_False, C_True) == "yes":
                 res = "yes"
             return res
         else:
+            #choose the best attribute by entropy
             self.best = self.ChooseAttribute(self.examples, C_False, C_True, self.attributes)
+            #get all the features
             features = self.attributes[self.best]
             index = self.F2I[self.best]
+            #for every feature in the attribute
             for v_i in features:
                 examples_i = list(filter(lambda x: x[0][index] == v_i, self.examples))
+                #c_f and c_t is sum of "no" classification and sum of "yes" classifications
                 c_f, c_t = self.checkClassification(self.examples)
+                #remove the best attribute from attributes
                 newAtt = copy.deepcopy(self.attributes)
                 del newAtt[self.best]
+                #recursive the new examples with new attributes(without best)
                 dtl = ID3(self.F2I, newAtt,self.Mode(c_f, c_t),examples_i)
+                #call to subtree (will be yes or no at the end)
                 subtree = dtl.DTL()
+                #add the node to the tree
                 self.addNode(Node(v_i, subtree))
             return self
 
     def get_Features(self, examples, attribute):
-        index = F2I[attribute]
+        """
+        get only relevent features for the attribute
+        :param examples: the relevent examples
+        :param attribute: attribute
+        :return:only relevent features for the attribute
+        """
+        index = self.F2I[attribute]
         feat = set([examples[i][0][index] for i in range(len(examples))])
         return feat
 
     def ChooseAttribute(self, examples, C_False, C_True, attributes):
+        """
+        choose the best attribute by entropy (and calculate the gain)
+        :param examples: all the relevent examples
+        :param C_False: how many "no" examples
+        :param C_True: how many "yes" examples
+        :param attributes: all the attributes
+        :return: best attribute
+        """
         # the first entropy
         total = C_True + C_False
         max_attribute = ""
         maxentropy = 0
+        #check foreach attribute
         for attribute in attributes:
+            #calc the first entropy over the data
             entropy = self.entropy(C_False, C_True)
+            #get all features for the attribute
             features = self.attributes[attribute]
+            #get the index of the attribute
             index = self.F2I[attribute]
             for feature in features:
+                #calc per feature how many yes and no examples
                 c_f, c_t, total_f, total_t= self.CalcPerFeature(examples, index, feature)
+                #calc the entropy
                 entropy -= ((c_t + c_f) / total) * self.entropy(c_f, c_t)
                 #print(entropy,c_f,c_t,total)
+            #change the max entropy and "best" attribute
             if entropy > maxentropy:
                 maxentropy = entropy
                 max_attribute = attribute
@@ -71,11 +111,23 @@ class ID3:
         return max_attribute
 
     def addNode(self, node):
+        """
+        add the node to the tree
+        :param node: node
+        :return: nothing
+        """
         if not node in self.tree:
             self.tree.append(node)
         return
 
     def CalcPerFeature(self, examples, index, feature):
+        """
+        calculate how many yes and no examples there are per feature
+        :param examples: all relevent examples
+        :param index: the attribute index
+        :param feature: the feature
+        :return: how many: no examples,yes examples,total no examples,total yes examples
+        """
         result_True = (list(filter(lambda x: x[1] == True, examples)))
         result_False = (list(filter(lambda x: x[1] == False, examples)))
         total_f = len(result_False)
@@ -86,20 +138,34 @@ class ID3:
 
     @staticmethod
     def entropy(C_False, C_True):
+        """
+        calculate the entropy
+        :param C_False: how many no examples
+        :param C_True: how many yes examples
+        :return: the entropy
+        """
         total  = C_False + C_True
+        #there are no examples
         if total == 0:
             return 0
         t_ex = C_True / total
         f_ex = C_False / total
         if f_ex == 0 and t_ex == 0:
             return 0
+        #only no examples
         elif t_ex == 0:
             return -(f_ex * math.log2(f_ex))
+        #only yes examples
         elif f_ex == 0:
             return -(t_ex * math.log2(t_ex))
         return -(t_ex * math.log2(t_ex) + f_ex * math.log2(f_ex))
     @staticmethod
     def checkClassification(examples):
+        """
+        check how many yes and no examples
+        :param examples: all relevent examples
+        :return: how many yes and no examples
+        """
         C_True = 0
         C_False = 0
         for example in examples:
@@ -110,6 +176,11 @@ class ID3:
         return C_False, C_True
 
     def Mode(self, C_False, C_True):
+        """
+        :param C_False: how many no examples
+        :param C_True: how many yes examples
+        :return: yes or no
+        """
         res = 'no'
         if C_True >= C_False:
             res = 'yes'
@@ -117,6 +188,12 @@ class ID3:
 
     @staticmethod
     def print_tree(tree, i=0):
+        """
+        print tree to screen (not necessary for this assignment)
+        :param tree: the tree we want to print
+        :param i: the index of the tree
+        :return: nothing
+        """
         subtrees = list(tree.tree)
         subtrees.sort(key=ID3.sort_by_label)
         for subtree_index in range(len(subtrees)):
@@ -137,10 +214,24 @@ class ID3:
 
     @staticmethod
     def sort_by_label(subtree):
+        """
+        sort the tree by label
+        :param subtree: the tree
+        :return: sort of the tree
+        """
         return subtree.label
 
     @staticmethod
     def predict(tree, F2I, example, attributes,default):
+        """
+        predict for "yes" or "no" for the example
+        :param tree: the tree
+        :param F2I: feature to index dictionary
+        :param example: the example we want to check
+        :param attributes: a dictionary for the fearure and the attributes
+        :param default: the default answer (if there is no attribute in F2I)
+        :return: the prediction for the example
+        """
         if tree == 'yes' or tree == 'no':
             return tree
         best_index = F2I[tree.best]
@@ -148,8 +239,17 @@ class ID3:
         subtree_labels = [subtree.label for subtree in tree.tree]
         subtree_index = subtree_labels.index(example_best)
         return ID3.predict(tree.tree[subtree_index].subtree, F2I, example,attributes,default)
+
     @staticmethod
     def get_accuracy(tree, test, F2I, attributes,default):
+        """
+        :param tree: the tree
+        :param test: the test-ser
+        :param F2I: feature to index dictionary
+        :param attributes: a dictionary for the fearure and the attributes
+        :param default: the default answer (if there is no attribute in F2I)
+        :return: the accuracy over the test-set
+        """
         accuracy = 0
         for row in test:
             label_t_f = row[1]
@@ -162,6 +262,11 @@ class ID3:
         return accuracy/len(test)
 
 def ID3_print_Tree(train_p):
+    """
+    print the tree
+    :param train_p:the train-set
+    :return: print the tree
+    """
     all_ex, att = make_examples(copy.deepcopy(train_p))
     F2I = parseAttributes(train_p[0])
     default, n = max_can_eat(all_ex)
@@ -175,6 +280,13 @@ def ID3_print_Tree(train_p):
 
 
 def ID3_k_folds(train_p):
+    """
+    k- folds for ID3, get data-set and divide to k (k=5)
+    return the avarage accuracy over all the the k times we runs the algorithm
+    each time one fold is the test-set and the other folds are train set
+    :param train_p: all the data
+    :return: avarage accuracy over all the the k times
+    """
     all_ex, att = make_examples(copy.deepcopy(train_p))
     F2I = parseAttributes(train_p[0])
     k=5
@@ -199,10 +311,14 @@ def ID3_k_folds(train_p):
     return avg_acu,tree
 
 def calc_ID3(train_p,dev_p):
+    """
+    :param train_p: train-set
+    :param dev_p: dev-set
+    :return:the accuracy (and the tree)
+    """
     train, att = make_examples(copy.deepcopy(train_p))
     dev,att_dev = make_examples(copy.deepcopy(dev_p))
     F2I = parseAttributes(train_p[0])
-    accuracy = 0
     default, n = max_can_eat(train)
     mode = "no"
     if default:
@@ -214,16 +330,14 @@ def calc_ID3(train_p,dev_p):
     print("ID3 : " + str(avg_acu))
     return avg_acu,tree
 
-
-
-def write_tree_writer(tree,file):
-    with open(file,"a") as f:
-        write_tree(tree,f)
-    f.close()
-
-
-
 def write_tree(tree,file,i=0):
+    """
+    write the tree for file as a tree
+    :param tree: the tree
+    :param file: the file (have to be open and can write in)
+    :param i: the iteration (over the tree)
+    :return: nothing - write into the file
+    """
     subtrees = list(tree.tree)
     subtrees.sort(key=ID3.sort_by_label)
     for subtree_index in range(len(subtrees)):
